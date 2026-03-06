799e714a3007b8c3ed1f7c27dbfe1ceadd425009
unknown <monirhrabby.personal@gmail.com>
2026-03-06 11:03:18 +0600

    developed: group-rule-accordion

--- app/(website)/(features)/groups/(feed)/view/[username]/about/page.tsx
+++ app/(website)/(features)/groups/(feed)/view/[username]/about/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import GroupAboutMemberSection from "@/components/shared/features/group/about/group-about-members-section";
 import { GroupAboutSection } from "@/components/shared/features/group/about/group-about-section";
+import GroupRulesAccordion from "@/components/shared/features/group/rules/group-rules-accordion";
 import { redirect } from "next/navigation";
 
 const Page = async ({ params }: { params: { username: string } }) => {
@@ -17,6 +18,11 @@ const Page = async ({ params }: { params: { username: string } }) => {
 
       <GroupAboutMemberSection
         username={username}
+        accessToken={cu.user.accessToken}
+      />
+
+      <GroupRulesAccordion
+        groupUserName={username}
         accessToken={cu.user.accessToken}
       />
     </div>
